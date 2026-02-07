2045d1f9ab8b3a4d4251946fc252a84c8f80974b
Kevin Tchoffa <ktcho@uic.edu>
2026-02-07 00:00:02 -0600

    Add home and onboarding routes from login screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Auth from "./pages/Auth.jsx";
+import Home from "./pages/Home.jsx";
+import Onboarding from "./pages/Onboarding.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/auth" element={<Auth />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/onboarding" element={<Onboarding />} />
         <Route path="*" element={<Navigate to="/auth" replace />} />
       </Routes>
     </BrowserRouter>
